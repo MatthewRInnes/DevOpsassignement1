44e2ef48ae0958c7caa863735d40b9b01eef9e65
MatthewRInnes <matthewinnes45@outlook.com>
2026-03-04 13:45:54 +0000

    Fix pounds-to-kg bug

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def main_conversion_function(value, from_unit, to_unit):
     elif from_unit == 'kg' and to_unit == 'pounds':
         return kg_to_pounds(value)
     elif from_unit == 'pounds' and to_unit == 'kg':
-        return -pounds_to_kg(value)  # BUG on purpose so the CI test fails
+        return pounds_to_kg(value)  # BUG on purpose so the CI test fails
     elif from_unit == 'grams' and to_unit == 'pounds':
         return grams_to_pounds(value)
     elif from_unit == 'pounds' and to_unit == 'grams':
